bbe4703770aa77782793790c20b3a75b91d78200
Bryan Rodriguez <brodriguez@grupo-link.com>
2020-01-20 17:35:07 -0500

    initial commit

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componente temp'),
+      ),
+      body: ListView(
+        // children: <Widget>[
+          
+        // ],
+      )
+    ); 
+  }
+}
