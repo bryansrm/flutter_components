2e869cb23339758dd1e0b62d7184000903c7b084
Bryan Rodriguez <bsteven1993@gmail.com>
2020-01-21 14:56:54 -0500

    se agrega diseño a las tarjetas

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -13,7 +13,16 @@ class CardPage extends StatelessWidget {
         children: <Widget>[
           _cardTipo1(),
           SizedBox(height: 30.0),
-          _cardTipo2()
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
         ],
       ),
     );
@@ -22,8 +31,11 @@ class CardPage extends StatelessWidget {
   Widget _cardTipo1() {
 
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
       child: Column(
         children: <Widget>[
+          SizedBox(height: 15.0,),
           ListTile(
             leading: Icon( Icons.photo_album, color: Colors.blue ),
             title: Text('Custom title'),
@@ -54,7 +66,7 @@ class CardPage extends StatelessWidget {
 
   Widget _cardTipo2(){
 
-    return Card(
+    final Widget card =  Container(
       child: Column(
         children: <Widget>[
           FadeInImage(
@@ -72,6 +84,25 @@ class CardPage extends StatelessWidget {
             child: Text('Mensaje de prueba')
           )
         ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: card,
       ),
     );
 
