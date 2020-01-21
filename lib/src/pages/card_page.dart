ff68b59ed413616a4cd07a1de030cb10d785d9d7
Bryan Rodriguez <bsteven1993@gmail.com>
2020-01-21 11:57:27 -0500

    creacion tipo tarjeta

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -8,10 +8,46 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Card Page'),
       ),
-      body: Center(
-        child: Text('Card Page'),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
       ),
     );
+  }
+
+  Widget _cardTipo1() {
+
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon( Icons.photo_album, color: Colors.blue ),
+            title: Text('Custom title'),
+            subtitle: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: (){
+
+                },
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: (){
+
+                },
+              )
+            ],
+          )
+        ],
+      ),
+    );
+
   }
 
 }
