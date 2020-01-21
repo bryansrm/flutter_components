a68ec7719316715b1f4147de9342b00b57113c60
Bryan Rodriguez <bsteven1993@gmail.com>
2020-01-21 11:45:59 -0500

    creacion pantalla cards

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
     'homePage': ( context ) => HomePage(),
     'alert'   : ( context ) => AlertPage(),
     'avatar'  : ( context ) => AvatarPage(),
+    'card'    : ( context ) => CardPage(),
   };
 }
