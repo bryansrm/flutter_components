465e347fed325da6840fa97623985f2ae0cd674e
Bryan Rodriguez <bsteven1993@gmail.com>
2020-01-20 20:42:20 -0500

    add providers json menu

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/providers/menu_providers.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+ 
+    print(menuProvider.opciones);
+
+    return ListView(
+      children: _listaItems(),
+    );
+  }
+
+  List<Widget> _listaItems() {
+
+    return [ 
+      ListTile(title: Text('Hola Mundo!'),),
+      Divider(),
+      ListTile(title: Text('Hola Mundo!'),),
+      Divider(),
+      ListTile(title: Text('Hola Mundo!'),),
+    ];
+  }
+
+}
---
+++ lib/src/providers/menu_providers.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart' show rootBundle;
+
+
+class _MenuProvider{
+
+  List<dynamic>  opciones = [];
+
+  _MenuProvider(){
+    cargaData();
+  }
+
+  cargaData() {
+
+    rootBundle.loadString('data/menu_opts.json')
+    .then( (data) {
+      print( data );
+    });
+
+  }
+
+}
+
+final menuProvider = _MenuProvider();
